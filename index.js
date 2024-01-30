d70936b60cf5fe521b170d1ae019b295d0801266
alex usman <alexusman603@gmail.com>
2024-01-30 15:14:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let open=document.querySelector("#open"); 
+    let close=document.querySelector("#close");
+    let ul= document.querySelector("#ul");
+
+    open.addEventListener("click", function () {
+        if (open.classList.contains("show")) {
+            console.log("open");
+
+            open.classList.remove("show");
+            open.classList.add("hide");
+            ul.classList.remove("ulHide");
+            close.classList.add("show");
+            
+        }
+    })
+        close.addEventListener("click", function () {
+        if (close.classList.contains("hide")) {
+            console.log("close");
+
+           close.classList.remove("show"); 
+           open.classList.add("show");
+           open.classList.remove("hide");
+           ul.classList.add("ulHide");
+        }
+    })
+    
